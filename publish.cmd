329219b684d638ea21973a2cecfd36c13199cbbc
Aviral Garg <contact@aviralgarg.com>
2020-01-30 17:27:16 -0800

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=react-knowledgebank-bot-Web-Deploy.pubxml -p:Password=AubZX8uggjxmumWbuNNlRv0olFpyTf3nj6x4ZYNKoq7kH67we4AtZpufp5vm
+
